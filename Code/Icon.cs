201c9fabde70594b24a3e1250f539a36e0968fad
Sami Kojo <sami.kojo@tuni.fi>
2026-01-19 19:01:53 +0200

    Move the icon

--- Code/Icon.cs
+++ Code/Icon.cs
@@ -8,12 +8,23 @@ namespace GA.GitWorkshop
 	/// </summary>
 	public partial class Icon : Sprite2D
 	{
+		[Export] private float _moveSpeed = 50f;
+
 		/// <summary>
 		/// Introduce yourself!
 		/// </summary>
 		public override void _Ready()
 		{
 			GD.Print("I'm an icon, yay!");
+		}
+
+		/// <summary>
+		/// Make the icon look alive.
+		/// </summary>
+		/// <param name="delta">Time passed since the last frame.</param>
+		public override void _Process(double delta)
+		{
+			Position += new Vector2(_moveSpeed * (float)delta, 0);
 		}
 	}
 }
