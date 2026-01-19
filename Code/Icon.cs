fab2572ddf6f0f3f9c9543d734ad500f0d8cb2f1
Sami Kojo <sami.kojo@tuni.fi>
2026-01-19 18:48:56 +0200

    Make the icon spin.

--- Code/Icon.cs
+++ Code/Icon.cs
@@ -8,12 +8,24 @@ namespace GA.GitWorkshop
 	/// </summary>
 	public partial class Icon : Sprite2D
 	{
+		// The rotation speed of the icon. Degrees per second.
+		[Export] private float _rotationSpeed = 45f;
+
 		/// <summary>
 		/// Introduce yourself!
 		/// </summary>
 		public override void _Ready()
 		{
 			GD.Print("I'm an icon, yay!");
+		}
+
+		/// <summary>
+		/// Functionality for the icon. Make it lively!
+		/// </summary>
+		/// <param name="delta"></param>
+		override public void _Process(double delta)
+		{
+			RotationDegrees += _rotationSpeed * (float)delta;
 		}
 	}
 }
